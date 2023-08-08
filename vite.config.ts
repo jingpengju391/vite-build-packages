e3c64f8083ffb98d8041c38e9be3d93af3351f48
jingpengju391 <757769998@qq.com>
2023-08-08 17:21:05 +0800

    delete useless

--- vite.config.ts
+++ vite.config.ts
@@ -92,16 +92,13 @@ export default defineConfig({
     compression({ algorithm: 'brotliCompress', exclude: [/\.(br)$/, /\.(gz)$/, /\.(map)$/, 'types'] }),
     AutoImport({
       // Auto import functions from Vue, e.g. ref, reactive, toRef...
-      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
       imports: ['vue'],
     
       // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
-      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
       resolvers: [
         ElementPlusResolver(),
     
         // Auto import icon components
-        // 自动导入图标组件
         IconsResolver({
           prefix: 'Icon'
         }),
@@ -111,12 +108,10 @@ export default defineConfig({
     Components({
       resolvers: [
         // Auto register icon components
-        // 自动注册图标组件
         IconsResolver({
           enabledCollections: ['ep'],
         }),
         // Auto register Element Plus components
-        // 自动导入 Element Plus 组件
         ElementPlusResolver(),
       ],
       dts: './components.d.ts',
