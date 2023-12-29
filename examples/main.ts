b7d945393e38cd26d52ab095798326629872e5f4
jingpengju391 <757769998@qq.com>
2023-12-29 17:10:50 +0800

    examples global import vxe-table

--- examples/main.ts
+++ examples/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import VXETable from 'vxe-table'
 import App from './App.vue'
+import 'vxe-table/lib/style.css'
 import './assets/css/normalize.css'
 const app = createApp(App)
+app.use(VXETable)
 app.mount('#app')
